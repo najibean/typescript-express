6b75f272b982ab792ef1f3d500fc0077f0a55625
najibean <najibullah89@gmail.com>
2022-10-08 22:19:24 +0800

    add bodyParser

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 class App {
   public app: Application;
 
   constructor() {
     this.app = express();
+    this.plugins();
     this.routes();
+  }
+
+  protected plugins(): void {
+    this.app.use(bodyParser.json());
   }
 
   protected routes(): void {
     this.app.route('/').get((req: Request, res: Response) => {
       return res.send('ini adalah route menggunakan typescript');
+    });
+
+    this.app.route('/users').post((req: Request, res: Response) => {
+      return res.send(req.body);
     });
   }
 }
