f2a1d319de99450c025f4857cf5614e2ff931848
najibean <najibullah89@gmail.com>
2022-10-09 15:05:39 +0800

    add middleware

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,11 +1,12 @@
 import BaseRoutes from './base.router';
+import { auth } from '../middlewares/auth.middleware';
 
 // controllers
 import UserController from '../controllers/user.controller';
 
 class UserRoutes extends BaseRoutes {
   public routes(): void {
-    this.router.get('/', UserController.index);
+    this.router.get('/', auth, UserController.index);
     this.router.post('/', UserController.create);
     this.router.get('/:id', UserController.show);
     this.router.put('/:id', UserController.update);
